6f29f2043019dc4ba0d183e3d7259cfa57ba1225
wo13834632341 <2968629681@qq.com>
2022-08-11 18:16:15 +0800

    最左边的

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,33 +1,47 @@
 <template>
 <div>
   <div style="height:400px;display: flex">
-    <el-table
-      class="up"
-      :data="first"
-      height="100%"
-      border
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="80">
+    <div>
+      <el-table
+        class="up"
+        :data="first"
+        height="90%"
+        border
+        style="width: 100%">
+        <el-table-column
+          prop="name"
+          label="交易对名称"
+          width="200">
+        </el-table-column>
+        <el-table-column
+        fixed="right"
+        label="操作"
+        width="120">
+        <template slot-scope="scope">
+          <el-button
+            @click.native.prevent="deleteRow(scope.$index, tableData)"
+            type="text"
+            size="small">
+            移除
+          </el-button>
+        </template>
       </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="80">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
-      <el-table-column label="操作" width="100">
-          <template slot-scope="scope">
-            <el-button size="mini" type="danger" plain @click="delmembers(scope.$index, scope.row)">删除</el-button>
-          </template>
-      </el-table-column>
-    </el-table>
-    <i class="el-icon-circle-plus-outline" @click="addmembers"></i>
+      </el-table>
+      <el-button @click="dialogFormVisible = true" height="10%" style="align-self: flex-end;">新增交易对</el-button>
+     </div>
+
+      <!-- 用来新增交易对的表单 -->
+     <el-dialog title="新增交易对" :visible.sync="dialogFormVisible">
+      <el-form :model="trade">
+        <el-form-item label="交易对名称" :label-width="formLabelWidth">
+          <el-input v-model="trade.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addmembers()">确 定</el-button>
+      </div>
+    </el-dialog>
 
     <el-table
       class="up"
@@ -105,7 +119,10 @@
 export default {
     data(){
         return {
-          first:[{}],
+          formLabelWidth: '120px',
+          dialogFormVisible: false,
+          trade:{name:''},
+          first:[{name:''}],
           second:[{}],
           third:[{}],
           tableData: [{
@@ -151,157 +168,212 @@ export default {
 			this.websock.close(); //离开路由之后断开websocket连接
 		},
     methods: {
-      // 动态添加
-      addmembers() {
-        console.log('添加');
-        var member = this.data.tableData;
-        console.log(member);
-        var length = member.length;
-        this.zichandetail.members.push({
-              id: parseInt(length),
-              date: '',
-              name: '',
-              address: '',
-        })
-      },
-      // 删除
-      delmembers(index, row) {
-        var that = this;
-        this.$confirm('确认删除吗?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-        }).then(() => {
-            //点击确定的操作(调用接口)
-            var hasmembers = that.zichandetail.members;
-            for (var i = 0; i < hasmembers.length; i++) {
-                if (row.id == hasmembers[i].id) {
-                    that.zichandetail.members.splice(i, 1);
-                    // this.$message({ message: '删除成功', duration: 2000, type: 'success' });
+        // 动态添加
+        addmembers() {
+          console.log(this.$data.trade.name)
+          var that=this
+          this.$axios({
+                url:'/api/symbols/check',
+                method:'post',
+                headers: {
+                  Authorization : that.$store.state.token
+                },
+                data:{
+                  symbol:that.$data.trade.name
                 }
-            }
-          }).catch(() => {
-          //点取消的提示
-              return;
-          });
+          }).then(function(){
+              that.$message.error('新增交易对失败，请确认该交易对是否已存在');
+            }).catch(function(){
+              that.$data.dialogFormVisible=false
+              that.$message({
+                message: '新增交易对成功',
+                type: 'success'
+              });
+              var transient=Object.assign({},that.$data.first)
+              transient.push({name:that.$data.trade.name})
+              var updata=[]
+              for (let index = 0; index < transient.length; index++) {
+                updata[index] =  transient[index].name;
+              }
+              that.$data.trade.name=''
+              transient.push({name:'123123'})
+              transient.push({name:'4124124'})
+              that.$axios({
+                url:'/api/user/set/symbols',
+                method:'post',
+                headers: {
+                  Authorization : that.$store.state.token
+                },
+                data:{
+                  symbols:updata
+                }
+              }).then(function(){
+                console.log('新增');
+                that.addfirst()
+              }).catch(function(err){
+                console.log(err);
+                console.log('新增错误');
+              })
+          })
         },
+        // 删除
+        deleteRow(index) {
+          console.log(index);
+          var that = this;
+          this.$confirm('确认删除吗?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+          }).then(() => {
+              //点击确定的操作(调用接口)
+              var updata=[]
+              var j=0
+              for (let i = 0; i < that.$data.first.length; i++) {
+                if(i == index) i++
+                updata[j] =  that.$data.first[i].name;
+                j++
+              }
+              that.$axios({
+                url:'/api/user/set/symbols',
+                method:'post',
+                headers: {
+                  Authorization : that.$store.state.token
+                },
+                data:{
+                  symbols:updata
+                }
+              }).then(function(){
+                that.addfirst()
+              }).catch(function(err){
+                console.log(err);
+              })
+              
+            }).catch(() => {
+            //点取消的提示
+                return;
+            });
+          },
 
-      currentTime() {
-				setInterval(this.formatDate, 500);
-			},
-			initWebSocket() {
-				//初始化weosocket
-				const wsuri = "ws://trader.niuwx.cn/ws/ping";
-				this.websock = new WebSocket(wsuri);
-				// 客户端接收服务端数据时触发
-				this.websock.onmessage = this.websocketonmessage;
-				// 连接建立时触发
-				this.websock.onopen = this.websocketonopen;
-				// 通信发生错误时触发
-				this.websock.onerror = this.websocketonerror;
-				// 连接关闭时触发
-				this.websock.onclose = this.websocketclose;
-			},
-			// 连接建立时触发
-			websocketonopen() {
-				//开启心跳
-				this.start();
-				//连接建立之后执行send方法发送数据
-				// let actions = {"room":"007854ce7b93476487c7ca8826d17eba","info":"1121212"};
-				// this.websocketsend('ping');
-			},
-			// 通信发生错误时触发
-			websocketonerror() {
-				console.log("出现错误");
-				this.reconnect();
-			},
-			// 客户端接收服务端数据时触发
-			websocketonmessage(e) {
-				console.log(e.data);
-        console.log("收到后台消息");
-				//收到服务器信息，心跳重置
-				this.reset();
-			},
-			websocketsend(Data) {
-				//数据发送
-				this.websock.send(Data);
-			},
-			// 连接关闭时触发
-			websocketclose(e) {
-				//关闭
-				console.log("断开连接", e);
-				//重连
-				this.reconnect();
-			},
-			reconnect() {
-				//重新连接
-				var that = this;
-				if (that.lockReconnect) {
-					return;
-				}
-        console.log('重新连接');
-				that.lockReconnect = true;
-				//没连接上会一直重连，设置延迟避免请求过多
-				that.timeoutnum && clearTimeout(that.timeoutnum);
-				that.timeoutnum = setTimeout(function () {
-					//新连接
-					that.initWebSocket();
-					that.lockReconnect = false;
-				}, 5000);
-			},
-			reset() {
-				//重置心跳
-				var that = this;
-				//清除时间
-				clearTimeout(that.timeoutObj);
-				clearTimeout(that.serverTimeoutObj);
-				//重启心跳
-				that.start();
-			},
-			start() {
-				//开启心跳
-				console.log("开启心跳");
-				var self = this;
-				self.timeoutObj && clearTimeout(self.timeoutObj);
-				self.serverTimeoutObj && clearTimeout(self.serverTimeoutObj);
-				self.timeoutObj = setTimeout(function () {
-          console.log(self.websock);
-					//这里发送一个心跳，后端收到后，返回一个心跳消息，
-					if (self.websock.readyState == 1) {
-            console.log("接受到了数据");
-						//如果连接正常
-						self.websock.send("ping"); //这里可以自己跟后端约定
-					} else {
-						//否则重连
-						self.reconnect();
-					}
-					self.serverTimeoutObj = setTimeout(function () {
-						//超时关闭
-						self.websock.close();
-					}, self.timeout);
-				}, self.timeout);
-			}
+        currentTime() {
+          setInterval(this.formatDate, 500);
+        },
+        initWebSocket() {
+          //初始化weosocket
+          const wsuri = "ws://trader.niuwx.cn/ws/ping";
+          this.websock = new WebSocket(wsuri);
+          // 客户端接收服务端数据时触发
+          this.websock.onmessage = this.websocketonmessage;
+          // 连接建立时触发
+          this.websock.onopen = this.websocketonopen;
+          // 通信发生错误时触发
+          this.websock.onerror = this.websocketonerror;
+          // 连接关闭时触发
+          this.websock.onclose = this.websocketclose;
+        },
+        // 连接建立时触发
+        websocketonopen() {
+          //开启心跳
+          this.start();
+          //连接建立之后执行send方法发送数据
+          // let actions = {"room":"007854ce7b93476487c7ca8826d17eba","info":"1121212"};
+          // this.websocketsend('ping');
+        },
+        // 通信发生错误时触发
+        websocketonerror() {
+          console.log("出现错误");
+          this.reconnect();
+        },
+        // 客户端接收服务端数据时触发
+        websocketonmessage(e) {
+          console.log(e.data);
+          console.log("收到后台消息");
+          //收到服务器信息，心跳重置
+          this.reset();
+        },
+        websocketsend(Data) {
+          //数据发送
+          this.websock.send(Data);
+        },
+        // 连接关闭时触发
+        websocketclose(e) {
+          //关闭
+          console.log("断开连接", e);
+          //重连
+          this.reconnect();
+        },
+        reconnect() {
+          //重新连接
+          var that = this;
+          if (that.lockReconnect) {
+            return;
+          }
+          console.log('重新连接');
+          that.lockReconnect = true;
+          //没连接上会一直重连，设置延迟避免请求过多
+          that.timeoutnum && clearTimeout(that.timeoutnum);
+          that.timeoutnum = setTimeout(function () {
+            //新连接
+            that.initWebSocket();
+            that.lockReconnect = false;
+          }, 5000);
+        },
+        reset() {
+          //重置心跳
+          var that = this;
+          //清除时间
+          clearTimeout(that.timeoutObj);
+          clearTimeout(that.serverTimeoutObj);
+          //重启心跳
+          that.start();
+        },
+        start() {
+          //开启心跳
+          console.log("开启心跳");
+          var self = this;
+          self.timeoutObj && clearTimeout(self.timeoutObj);
+          self.serverTimeoutObj && clearTimeout(self.serverTimeoutObj);
+          self.timeoutObj = setTimeout(function () {
+            console.log(self.websock);
+            //这里发送一个心跳，后端收到后，返回一个心跳消息，
+            if (self.websock.readyState == 1) {
+              console.log("接受到了数据");
+              //如果连接正常
+              self.websock.send("ping"); //这里可以自己跟后端约定
+            } else {
+              //否则重连
+              self.reconnect();
+            }
+            self.serverTimeoutObj = setTimeout(function () {
+              //超时关闭
+              self.websock.close();
+            }, self.timeout);
+          }, self.timeout);
+        },
+        addfirst(){
+          var that =this
+          if(this.$store.state.token == '') this.$router.replace({name:'login'})
+          console.log(this.$store.state.token);
+          this.$axios({
+                url:'/api/symbols/get',
+                method:'post',
+                headers: {
+                  Authorization : this.$store.state.token
+                }
+            }).then(function(res){
+              console.log(res.data);
+              for (let index = 0; index < res.data.symbols.length; index++) {
+              if(index<res.data.symbols.length-1) that.$data.first.push({name:''})
+              that.$data.first[index].name=res.data.symbols[index]
+              }
+          }).catch(function(err){
+            console.log(err);
+          })
+        }
       },
 		mounted() {
-			this.currentTime();
+			this.currentTime()
       // 请求第一个表格的数据
-      var that =this
-      if(this.$store.state.token == '') this.$router.replace({name:'login'})
-      console.log(this.$store.state.token);
-      this.$axios({
-            url:'/api/symbols/get',
-            method:'post',
-            headers: {
-              Authorization : this.$store.state.token
-            }
-        }).then(function(res){
-          console.log(res.data);
-          that.$data.first=res.data.symbols
-          that.$data.first[that.$data.first.length]=''
-          console.log(that.$data.first.length);
-      }).catch(function(err){
-        console.log(err);
-      })
+      this.addfirst()
 		},
 		// 销毁定时器
 		beforeDestroy() {
